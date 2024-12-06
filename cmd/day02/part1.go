e5a05b3187ae3be68dda4063c57bef8bd8b9f7f4
Felipe Moura <felipem@auctionedge.com>
2024-12-06 12:25:17 -0600

    first draft of the day 2 part 1

--- cmd/day02/part1.go
+++ cmd/day02/part1.go
@@ -2,22 +2,77 @@ package day02
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
+
+const MAX_DIFF = 3
 
 func part1(file []string) int {
 	fmt.Println("Day 02")
 	total := 0
 	for _, line := range file {
-		if processLine(line) {
+		fmt.Printf("Processing line: %s\n", line)
+		fmt.Printf("Result: %t\n", ProcessLine(line))
+		if ProcessLine(line) {
 			total += 1
 		}
 	}
 	return total
 }
 
-func processLine(line string) bool {
+func abs(n int) int {
+	if n < 0 {
+		return -n
+	}
+	return n
+}
+
+func passDiff(number1 string, number2 string, leftToRight bool) bool {
+	num1, err := strconv.Atoi(number1)
+	if err != nil {
+		panic(err)
+	}
+	num2, err := strconv.Atoi(number2)
+	if err != nil {
+		panic(err)
+	}
+	if leftToRight {
+		if num1 >= num2 {
+			return false
+		}
+	} else {
+		if num1 <= num2 {
+			return false
+		}
+	}
+	return abs(num1-num2) <= MAX_DIFF
+}
+
+func ProcessLine(line string) bool {
 	line = strings.TrimSpace(line)
 	numbers := strings.Split(line, " ")
-	fmt.Println(numbers)
+
+	num0, err := strconv.Atoi(numbers[0])
+	if err != nil {
+		panic(err)
+	}
+	num1, err := strconv.Atoi(numbers[1])
+	if err != nil {
+		panic(err)
+	}
+	if num0 == num1 {
+		return false
+	}
+	leftToRight := num0 < num1
+
+	for i, _ := range numbers {
+		if i == 0 {
+			continue
+		}
+		if !passDiff(numbers[i], numbers[i-1], leftToRight) {
+			return false
+		}
+	}
+	return true
 }
--- cmd/day02/part1_test.go
+++ cmd/day02/part1_test.go
@@ -5,16 +5,19 @@ import (
 	"testing"
 	"strings"
 )
+
+
 var input = `7 6 4 2 1
 1 2 7 8 9
 9 7 6 2 1
 1 3 2 4 5
 8 6 4 4 1
 1 3 6 7 9`
+// Per line: T F F F F T
 // Result for part 1: 2
 
 func TestPart1(t *testing.T) {
 	input := strings.Split(input, "\n")
-	result := part1(input)
-	fmt.Println(result)
+	fmt.Println("Day 02")
+	fmt.Println(ProcessLine(input[0]))
 }
